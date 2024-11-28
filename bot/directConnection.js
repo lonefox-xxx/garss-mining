14adb33ae7b922bb1701ade8b39dd53fef85f40e
lonefox <96380261+lonefox-xxx@users.noreply.github.com>
2024-11-28 12:10:33 +0530

    bug fixed

--- bot/directConnection.js
+++ bot/directConnection.js
@@ -64,7 +64,7 @@ function DirectconnectAndPing() {
                 `WebSocket closed with code: ${code}, reason: ${reason}`
             );
             setTimeout(
-                () => connectAndPing(USERID),
+                () => DirectconnectAndPing(),
                 retryInterval
             );
         });
